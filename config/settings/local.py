bad6963d2276c8fcb318b3a3063a034e734e74af
Sanjay <sanjaysingh13@gmail.com>
2025-09-22 21:49:20 +0530

    hack for RUNNING_O_AWS

--- config/settings/local.py
+++ config/settings/local.py
@@ -1,6 +1,20 @@
+# ruff: noqa: E501
+"""
+Local development settings for The Khaki Estate application.
+
+This file contains configuration for local development environment.
+It includes a production hack that allows running the app on AWS production
+machines by setting the RUNNING_ON_AWS environment variable to True.
+When this variable is set, the configuration switches to production-like settings
+including AWS S3 storage, security headers, and proper email backend.
+"""
+
+# Import all base settings and specific components needed for both local and production configurations
 from .base import *  # noqa: F403
+from .base import DATABASES
 from .base import INSTALLED_APPS
 from .base import MIDDLEWARE
+from .base import REDIS_URL
 from .base import env
 
 # GENERAL
@@ -10,10 +24,16 @@ DEBUG = True
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env(
     "DJANGO_SECRET_KEY",
-    default="ZNT1yrk87fX5imxmBeQoC9Wj9UTcv42YQAZG1AG3351d5cxiMwqS15eD1vICEzNC",
+    default="zVV15fLhs32NAPeeyNi3CgVpLZjEoVItWOx7nrpFNMwScCEe3HRk70YoQ4DWaV36",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]  # noqa: S104
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    "3.111.27.136",
+    "the-khaki-estate.com",
+    "www.the-khaki-estate.com",
+]
 
 # CACHES
 # ------------------------------------------------------------------------------
@@ -60,8 +80,213 @@ INSTALLED_APPS += ["django_extensions"]
 # Celery
 # ------------------------------------------------------------------------------
 # https://docs.celeryq.dev/en/stable/userguide/configuration.html#task-always-eager
-CELERY_TASK_ALWAYS_EAGER = True
+CELERY_TASK_ALWAYS_EAGER = False
 # https://docs.celeryq.dev/en/stable/userguide/configuration.html#task-eager-propagates
-CELERY_TASK_EAGER_PROPAGATES = True
+CELERY_TASK_EAGER_PROPAGATES = False
 # Your stuff...
 # ------------------------------------------------------------------------------
+
+# WE USE THIS HACK TO RUN THE APP ON LOCALHOST EVEN IN PRODUCTION
+# This try-except block allows the same local.py file to be used both for
+# local development and production deployment on AWS by checking for the
+# RUNNING_ON_AWS environment variable
+try:
+    # Check if we're running on AWS production environment
+    # This environment variable should be set to 'True' on production servers
+    RUNNING_ON_AWS = env("RUNNING_ON_AWS")
+    
+    # Switch to production mode: disable debug and set production database connection
+    DEBUG = False
+    DATABASES["default"]["CONN_MAX_AGE"] = env.int("CONN_MAX_AGE", default=60)
+
+    # CACHES
+    # ------------------------------------------------------------------------------
+    CACHES = {
+        "default": {
+            "BACKEND": "django_redis.cache.RedisCache",
+            "LOCATION": REDIS_URL,
+            "OPTIONS": {
+                "CLIENT_CLASS": "django_redis.client.DefaultClient",
+                # Mimicking memcache behavior.
+                # https://github.com/jazzband/django-redis#memcached-exceptions-behavior
+                "IGNORE_EXCEPTIONS": True,
+            },
+        },
+    }
+
+    # SECURITY
+    # ------------------------------------------------------------------------------
+    # https://docs.djangoproject.com/en/dev/ref/settings/#secure-proxy-ssl-header
+    SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+    # https://docs.djangoproject.com/en/dev/ref/settings/#secure-ssl-redirect
+    SECURE_SSL_REDIRECT = env.bool("DJANGO_SECURE_SSL_REDIRECT", default=True)
+    # https://docs.djangoproject.com/en/dev/ref/settings/#session-cookie-secure
+    SESSION_COOKIE_SECURE = True
+    # https://docs.djangoproject.com/en/dev/ref/settings/#session-cookie-name
+    SESSION_COOKIE_NAME = "__Secure-sessionid"
+    # https://docs.djangoproject.com/en/dev/ref/settings/#csrf-cookie-secure
+    CSRF_COOKIE_SECURE = True
+    # https://docs.djangoproject.com/en/dev/ref/settings/#csrf-cookie-name
+    CSRF_COOKIE_NAME = "__Secure-csrftoken"
+    # https://docs.djangoproject.com/en/dev/topics/security/#ssl-https
+    # https://docs.djangoproject.com/en/dev/ref/settings/#secure-hsts-seconds
+    # TODO: set this to 60 seconds first and then to 518400 once you prove the former works
+    SECURE_HSTS_SECONDS = 60
+    # https://docs.djangoproject.com/en/dev/ref/settings/#secure-hsts-include-subdomains
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = env.bool(
+        "DJANGO_SECURE_HSTS_INCLUDE_SUBDOMAINS",
+        default=True,
+    )
+    # https://docs.djangoproject.com/en/dev/ref/settings/#secure-hsts-preload
+    SECURE_HSTS_PRELOAD = env.bool("DJANGO_SECURE_HSTS_PRELOAD", default=True)
+    # https://docs.djangoproject.com/en/dev/ref/middleware/#x-content-type-options-nosniff
+    SECURE_CONTENT_TYPE_NOSNIFF = env.bool(
+        "DJANGO_SECURE_CONTENT_TYPE_NOSNIFF",
+        default=True,
+    )
+
+
+    # https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html#settings
+
+    # https://django-storages.readthedocs.io/en/latest/#installation
+    INSTALLED_APPS += ["storages"]
+
+    AWS_ACCESS_KEY_ID = env("DJANGO_AWS_ACCESS_KEY_ID")
+    # https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html#settings
+    AWS_SECRET_ACCESS_KEY = env("DJANGO_AWS_SECRET_ACCESS_KEY")
+    # https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html#settings
+    AWS_STORAGE_BUCKET_NAME = env("DJANGO_AWS_STORAGE_BUCKET_NAME")
+    # https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html#settings
+    AWS_QUERYSTRING_AUTH = False
+    # DO NOT change these unless you know what you're doing.
+    _AWS_EXPIRY = 60 * 60 * 24 * 7
+    # https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html#settings
+    AWS_S3_OBJECT_PARAMETERS = {
+        "CacheControl": f"max-age={_AWS_EXPIRY}, s-maxage={_AWS_EXPIRY}, must-revalidate",
+    }
+    # https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html#settings
+    AWS_S3_MAX_MEMORY_SIZE = env.int(
+        "DJANGO_AWS_S3_MAX_MEMORY_SIZE",
+        default=100_000_000,  # 100MB
+    )
+    # https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html#settings
+    AWS_S3_REGION_NAME = env("DJANGO_AWS_S3_REGION_NAME", default=None)
+    # https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html#cloudfront
+    AWS_S3_CUSTOM_DOMAIN = env("DJANGO_AWS_S3_CUSTOM_DOMAIN", default=None)
+    aws_s3_domain = AWS_S3_CUSTOM_DOMAIN or f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
+    # STATIC & MEDIA
+    # ------------------------
+    STORAGES = {
+        "default": {
+            "BACKEND": "storages.backends.s3.S3Storage",
+            "OPTIONS": {
+                "location": "media",
+                "file_overwrite": False,
+            },
+        },
+        "staticfiles": {
+            "BACKEND": "storages.backends.s3.S3Storage",
+            "OPTIONS": {
+                "location": "static",
+                "default_acl": "public-read",
+            },
+        },
+    }
+    MEDIA_URL = f"https://{aws_s3_domain}/media/"
+    COLLECTFASTA_STRATEGY = "collectfasta.strategies.boto3.Boto3Strategy"
+    STATIC_URL = f"https://{aws_s3_domain}/static/"
+
+    # EMAIL
+    # ------------------------------------------------------------------------------
+    # https://docs.djangoproject.com/en/dev/ref/settings/#default-from-email
+    DEFAULT_FROM_EMAIL = env(
+        "DJANGO_DEFAULT_FROM_EMAIL",
+        default="The Khaki Estate <noreply@the-khaki-estate.com>",
+    )
+    # https://docs.djangoproject.com/en/dev/ref/settings/#server-email
+    SERVER_EMAIL = env("DJANGO_SERVER_EMAIL", default=DEFAULT_FROM_EMAIL)
+    # https://docs.djangoproject.com/en/dev/ref/settings/#email-subject-prefix
+    EMAIL_SUBJECT_PREFIX = env(
+        "DJANGO_EMAIL_SUBJECT_PREFIX",
+        default="[The Khaki Estate] ",
+    )
+    ACCOUNT_EMAIL_SUBJECT_PREFIX = EMAIL_SUBJECT_PREFIX
+
+    # ADMIN
+    # ------------------------------------------------------------------------------
+    # Django Admin URL regex.
+    ADMIN_URL = env("DJANGO_ADMIN_URL")
+
+    # Anymail
+    # ------------------------------------------------------------------------------
+    # https://anymail.readthedocs.io/en/stable/installation/#installing-anymail
+    INSTALLED_APPS += ["anymail"]
+    # https://docs.djangoproject.com/en/dev/ref/settings/#email-backend
+    # https://anymail.readthedocs.io/en/stable/installation/#anymail-settings-reference
+    # https://anymail.readthedocs.io/en/stable/esps/brevo/
+    EMAIL_BACKEND = "anymail.backends.brevo.EmailBackend"
+    ANYMAIL = {
+        "BREVO_API_KEY": env("BREVO_API_KEY"),
+        "BREVO_API_URL": env("BREVO_API_URL", default="https://api.brevo.com/v3/"),
+    }
+
+    # Collectfasta
+    # ------------------------------------------------------------------------------
+    # https://github.com/jasongi/collectfasta#installation
+    INSTALLED_APPS = ["collectfasta", *INSTALLED_APPS]
+
+    # LOGGING
+    # ------------------------------------------------------------------------------
+    # https://docs.djangoproject.com/en/dev/ref/settings/#logging
+    # See https://docs.djangoproject.com/en/dev/topics/logging for
+    # more details on how to customize your logging configuration.
+    # A sample logging configuration. The only tangible logging
+    # performed by this configuration is to send an email to
+    # the site admins on every HTTP 500 error when DEBUG=False.
+    LOGGING = {
+        "version": 1,
+        "disable_existing_loggers": False,
+        "filters": {"require_debug_false": {"()": "django.utils.log.RequireDebugFalse"}},
+        "formatters": {
+            "verbose": {
+                "format": "%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s",
+            },
+        },
+        "handlers": {
+            "mail_admins": {
+                "level": "ERROR",
+                "filters": ["require_debug_false"],
+                "class": "django.utils.log.AdminEmailHandler",
+            },
+            "console": {
+                "level": "DEBUG",
+                "class": "logging.StreamHandler",
+                "formatter": "verbose",
+            },
+        },
+        "root": {"level": "INFO", "handlers": ["console"]},
+        "loggers": {
+            "django.request": {
+                "handlers": ["mail_admins"],
+                "level": "ERROR",
+                "propagate": True,
+            },
+            "django.security.DisallowedHost": {
+                "level": "ERROR",
+                "handlers": ["console", "mail_admins"],
+                "propagate": True,
+            },
+        },
+    }
+
+    # Tools that generate code samples can use SERVERS to point to the correct domain
+    SPECTACULAR_SETTINGS["SERVERS"] = [
+        {"url": "https://the-khaki-estate.com", "description": "Production server"},
+    ]
+
+except Exception as e:
+    # If any error occurs during production configuration setup,
+    # log the error and continue with local development settings
+    print(f"Production configuration error: {str(e)}")
+    # Your stuff...
+    # ------------------------------------------------------------------------------
